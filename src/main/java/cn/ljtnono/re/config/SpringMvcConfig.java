8837b7c705725100936a263ac8c0e8fc6c38689b
ljtnono <ljtnono@163.com>
2019-12-28 18:07:28 +0800

    添加了swagger组件

--- src/main/java/cn/ljtnono/re/config/SpringMvcConfig.java
+++ src/main/java/cn/ljtnono/re/config/SpringMvcConfig.java
@@ -1,25 +1,26 @@
 package cn.ljtnono.re.config;
 
 import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 /**
  * 配置SpringMvc处理静态文件
+ *
  * @author ljt
- * @date 2019/10/19
  * @version 1.0
+ * @date 2019/10/19
  */
 @SpringBootConfiguration
 public class SpringMvcConfig implements WebMvcConfigurer {
 
     /**
      * 配置SpringMvc静态资源拦截
+     *
      * @param registry 静态资源注册中心
      */
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/static/**")
-                .addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/static/**", "swagger-ui.html", "/webjars/**")
+                .addResourceLocations("classpath:/static/", "classpath:/META-INF/resources/", "classpath:/META-INF/resources/webjars/");
     }
 }
--- src/main/java/cn/ljtnono/re/controller/PageController.java
+++ src/main/java/cn/ljtnono/re/controller/PageController.java
@@ -4,6 +4,7 @@ import cn.ljtnono.re.entity.ReBlog;
 import cn.ljtnono.re.service.IReBlogService;
 import cn.ljtnono.re.util.StringUtil;
 import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,47 +12,50 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.servlet.http.HttpSession;
 
 /**
  * 处理页面路由的Controller
  * @author ljt
- * @date 2019/10/19
- * @version 1.0
+ * @date 2019/12/28
+ * @version 1.0.1
  */
 @Controller
+@ApiOperation("路由信息")
 public class PageController {
 
-    @Autowired
     private IReBlogService iReBlogService;
+
+    @Autowired
+    public PageController(IReBlogService iReBlogService) {
+        this.iReBlogService = iReBlogService;
+    }
 
     private Logger logger = LoggerFactory.getLogger(PageController.class);
 
-    @RequestMapping("/")
-    public String fore(ModelMap map, HttpSession session) {
+    @GetMapping("/")
+    @ApiOperation(value = "跳转到首页")
+    public String fore(ModelMap map) {
         map.addAttribute("currentPage", "index");
         return "fore/index";
     }
 
-    @RequestMapping("/{page}")
+    @GetMapping("/{page:^(?!swagger-ui.html).*$}")
     public String foreTemplates(@PathVariable final String page, final ModelMap map) {
         setActivePage(page, map);
         return "fore/" + page;
     }
 
-    @RequestMapping("/admin/login")
+    @GetMapping("/admin/login")
     public String toLogin() {
         return "back/login";
     }
 
-    @RequestMapping({"/admin", "/admin/"})
+    @GetMapping({"/admin", "/admin/"})
     public String back(ModelMap map) {
         return "back/index";
     }
 
-    @RequestMapping("/admin/{page}")
+    @GetMapping("/admin/{page:^(?!\\*\\.html)}")
     public String backTemplates(@PathVariable String page) {
         return "back/" + page;
     }
@@ -90,9 +94,7 @@ public class PageController {
             logger.info("博客id不能为空");
             return "forward:/error/404";
         }
-
         ReBlog byId = iReBlogService.getById(id);
-
         if (byId == null || byId.getStatus() == 0) {
             // 如果没有查询到或者该博客已经删除，那么返回404页面
             return "forward:/error/404";
@@ -101,19 +103,14 @@ public class PageController {
         modelMap.addAttribute("blog", byId);
         ReBlog next = iReBlogService.getById(Integer.parseInt(id) + 1);
         iReBlogService.update(new UpdateWrapper<ReBlog>().eq("id", byId.getId()).set("view", byId.getView() + 1));
-
         if (next != null) {
             modelMap.addAttribute("next", next);
         }
-
         ReBlog prev = iReBlogService.getById(Integer.parseInt(id) - 1);
-
         if (next != null) {
             modelMap.addAttribute("prev", prev);
         }
-
         modelMap.addAttribute("currentPage", "articles");
         return "fore/article";
     }
-
 }
