d29953ab2aadfc53921a18a2d47ed49437c1324c
ljtnono <ljtnono@163.com>
2019-12-29 20:15:48 +0800

    controller改用dto对象来传递并校验参数

--- src/main/java/cn/ljtnono/re/security/RePasswordEncoderImpl.java
+++ src/main/java/cn/ljtnono/re/security/RePasswordEncoderImpl.java
@@ -8,18 +8,18 @@ import org.springframework.stereotype.Component;
  * 加密器
  * @author ljt
  * @date 2019/11/18
- * @version 1.0
+ * @version 1.0.2
  */
 @Component("rePasswordEncoder")
 public class RePasswordEncoderImpl implements PasswordEncoder {
 
     @Override
     public String encode(CharSequence rawPassword) {
-        return EncryptUtil.getMd5LowerCase((String) rawPassword);
+        return EncryptUtil.getInstance().getMd5LowerCase((String) rawPassword);
     }
 
     @Override
     public boolean matches(CharSequence rawPassword, String encodedPassword) {
-        return encodedPassword.equals(EncryptUtil.getMd5LowerCase((String) rawPassword));
+        return encodedPassword.equals(EncryptUtil.getInstance().getMd5LowerCase((String) rawPassword));
     }
 }
