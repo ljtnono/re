f61af0e8ba539242886289817d49791ef76a6555
ljtnono <ljtnono@163.com>
2020-01-06 20:36:36 +0800

    添加restore功能

--- src/main/java/cn/ljtnono/re/controller/ReBlogTypeController.java
+++ src/main/java/cn/ljtnono/re/controller/ReBlogTypeController.java
@@ -4,6 +4,8 @@ import cn.ljtnono.re.dto.PageDTO;
 import cn.ljtnono.re.entity.ReBlogType;
 import cn.ljtnono.re.pojo.JsonResult;
 import cn.ljtnono.re.service.IReBlogTypeService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -13,13 +15,15 @@ import java.io.Serializable;
 
 /**
  * 博客类型Controller
+ *
  * @author ljt
- * @date 2019/12/23
- * @version 1.0.1
+ * @version 1.0.2
+ * @date 2020/1/6
  */
 @RestController
 @RequestMapping("/blog_type")
 @Slf4j
+@Api(value = "博客分类类型Controller", tags = {"博客类型Controller"})
 public class ReBlogTypeController {
 
     private IReBlogTypeService iReBlogTypeService;
@@ -30,38 +34,48 @@ public class ReBlogTypeController {
     }
 
     @GetMapping
+    @ApiOperation(value = "获取所有博客类型列表", httpMethod = "GET")
     public JsonResult listEntityAll() {
         return iReBlogTypeService.listEntityAll();
     }
-
 
     @PostMapping
     public JsonResult saveEntity(ReBlogType entity) {
         return iReBlogTypeService.saveEntity(entity);
     }
 
-
-    @PostMapping("/{id:\\d+}")
-    public JsonResult updateEntityById(@PathVariable(value = "id", required = false) Serializable id, ReBlogType entity) {
+    @PutMapping("/{id:\\d+}")
+    public JsonResult updateEntityById(@PathVariable(value = "id") Serializable id, ReBlogType entity) {
         return iReBlogTypeService.updateEntityById(id, entity);
     }
 
-
     @DeleteMapping("/{id:\\d+}")
-    public JsonResult deleteEntityById(@PathVariable(value = "id", required = false) Serializable id) {
+    @ApiOperation(value = "根据id删除一个博客类型", notes = "id只能为数字类型", httpMethod = "DELETE")
+    public JsonResult deleteEntityById(@PathVariable(value = "id") Serializable id) {
         return iReBlogTypeService.deleteEntityById(id);
     }
 
-
     @GetMapping("/{id:\\d+}")
-    public JsonResult getEntityById(@PathVariable(value = "id", required = false) Serializable id) {
+    public JsonResult getEntityById(@PathVariable(value = "id") Serializable id) {
         return iReBlogTypeService.getEntityById(id);
     }
-
 
     @GetMapping("/listBlogTypePage")
     public JsonResult listBlogTypePage(PageDTO pageDTO) {
         return iReBlogTypeService.listBlogTypePage(pageDTO.getPage(), pageDTO.getCount());
+    }
+
+    @PutMapping("/restore/{id:\\d+}")
+    @ApiOperation(value = "恢复删除的博客类型", notes = "id只能为数字类型", httpMethod = "PUT")
+    public JsonResult restore(@PathVariable(value = "id") Serializable id) {
+        return iReBlogTypeService.restore(id);
+    }
+
+    @PostMapping("/search")
+    @ApiOperation(value = "根据博客类型名字模糊查询", notes = "根据博客类型名模糊查询", httpMethod = "POST")
+    public JsonResult search(final String name) {
+
+        return null;
     }
 
 }
