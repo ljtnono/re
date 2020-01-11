0b50b5611f77632f44b3593fa620d11f7c5aa7ff
ljtnono <ljtnono@163.com>
2020-01-11 14:25:41 +0800

    增加验证

--- src/main/java/cn/ljtnono/re/dto/PageDTO.java
+++ src/main/java/cn/ljtnono/re/dto/PageDTO.java
@@ -1,8 +1,11 @@
 package cn.ljtnono.re.dto;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 /**
@@ -13,14 +16,19 @@ import java.io.Serializable;
  */
 @Data
 @ToString
+@NoArgsConstructor
 public class PageDTO implements Serializable {
 
     private static final long serialVersionUID = -3320275112750620130L;
 
     /** 请求的页数 */
+    @NotNull
+    @Min(value = 1, message = "请求页数不能小于1")
     private Integer page;
 
     /** 每页显示的条数 */
+    @NotNull
+    @Min(value = 10, message = "每页条数不能小于10")
     private Integer count;
 
 }
