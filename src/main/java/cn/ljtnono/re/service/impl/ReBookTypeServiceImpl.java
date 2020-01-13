dcb67072b9bb15810fc6c9446cfe5ae10fcd2944
ljtnono <ljtnono@163.com>
2020-01-13 21:13:57 +0800

    完成链接新增功能

--- src/main/java/cn/ljtnono/re/service/impl/ReBookTypeServiceImpl.java
+++ src/main/java/cn/ljtnono/re/service/impl/ReBookTypeServiceImpl.java
@@ -36,16 +36,6 @@ public class ReBookTypeServiceImpl extends ServiceImpl<ReBookTypeMapper, ReBookT
         this.redisUtil = redisUtil;
     }
 
-    /**
-     * 新增单个实体类
-     *
-     * @param entity 具体的实体类
-     * @return 返回操作结果
-     * 操作成功返回（如果有附加信息，那么通过fields字段带回，其中特别注意如果data为null，那么不返回)
-     * {request: "success", status: 200, message: "操作成功“}
-     * 操作失败返回
-     * {request: "fail", status: 具体错误码{@link GlobalErrorEnum}, message: 具体错误信息{@link GlobalErrorEnum}}
-     */
     @Override
     public JsonResult saveEntity(ReBookType entity) {
         Optional<ReBookType> optionalReBook = Optional.ofNullable(entity);
@@ -63,16 +53,6 @@ public class ReBookTypeServiceImpl extends ServiceImpl<ReBookTypeMapper, ReBookT
         }
     }
 
-    /**
-     * 根据id删除一个实体类
-     *
-     * @param id 实体类id
-     * @return 返回操作结果
-     * 操作成功返回（如果有附加信息，那么通过fields字段带回，其中特别注意如果data为null，那么不返回)
-     * {request: "success", status: 200, message: "操作成功“, data: {删除的实体类}}
-     * 操作失败返回
-     * {request: "fail", status: 具体错误码{@link GlobalErrorEnum}, message: 具体错误信息{@link GlobalErrorEnum}}
-     */
     @Override
     public JsonResult deleteEntityById(Serializable id) {
         Optional<Serializable> optionalId = Optional.ofNullable(id);
@@ -100,17 +80,6 @@ public class ReBookTypeServiceImpl extends ServiceImpl<ReBookTypeMapper, ReBookT
         }
     }
 
-    /**
-     * 根据id更新一个实体类
-     *
-     * @param id     实体类的id
-     * @param entity 要更新的实体类
-     * @return 返回操作结果
-     * 操作成功返回（如果有附加信息，那么通过fields字段带回，其中特别注意如果data为null，那么不返回)
-     * {request: "success", status: 200, message: "操作成功“}
-     * 操作失败返回
-     * {request: "fail", status: 具体错误码{@link GlobalErrorEnum}, message: 具体错误信息{@link GlobalErrorEnum}}
-     */
     @Override
     public JsonResult updateEntityById(Serializable id, ReBookType entity) {
         Optional<Serializable> optionalId = Optional.ofNullable(id);
@@ -138,16 +107,6 @@ public class ReBookTypeServiceImpl extends ServiceImpl<ReBookTypeMapper, ReBookT
         }
     }
 
-    /**
-     * 根据id获取一个实体类
-     *
-     * @param id 实体类id
-     * @return 返回操作结果
-     * 操作成功返回（如果有附加信息，那么通过fields字段带回，其中特别注意如果data为null，那么不返回)
-     * {request: "success", status: 200, message: "操作成功“, data: {实体类}}
-     * 操作失败返回
-     * {request: "fail", status: 具体错误码{@link GlobalErrorEnum}, message: 具体错误信息{@link GlobalErrorEnum}}
-     */
     @Override
     public JsonResult getEntityById(Serializable id) {
         Optional<Serializable> optionalId = Optional.ofNullable(id);
@@ -186,13 +145,6 @@ public class ReBookTypeServiceImpl extends ServiceImpl<ReBookTypeMapper, ReBookT
         }
     }
 
-    /**
-     * 获取实体类的所有列表
-     *
-     * @return 实体类所有列表
-     * 操作成功{request: "success", status: 200, message: "操作成功“, data: {列表}}
-     * 操作失败{request: "fail", status: 具体错误码{@link GlobalErrorEnum}, message: 具体错误信息{@link GlobalErrorEnum}}
-     */
     @Override
     public JsonResult listEntityAll() {
         List<ReBookType> reBookTypeList = list();
