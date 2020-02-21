de7dfdf457ecbfdaae37618301737b794f4473ef
ljtnono <ljtnono@163.com>
2020-02-21 23:36:51 +0800

    添加OSS工具类

---
+++ src/main/java/cn/ljtnono/re/aliyun/OSSConfig.java
@@ -0,0 +1,18 @@
+package cn.ljtnono.re.aliyun;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * 阿里云oss配置类
+ * @author ljt
+ * @date 2020/2/19
+ * @version 1.0.1
+ */
+@Component
+public class OSSConfig {
+
+    @Value("${server.port}")
+    private String title;
+
+}
---
+++ src/main/java/cn/ljtnono/re/aliyun/OSSUtil.java
@@ -0,0 +1,4 @@
+package cn.ljtnono.re.aliyun;
+
+public class OSSUtil {
+}
