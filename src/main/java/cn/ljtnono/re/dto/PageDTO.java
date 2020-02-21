92fd272caf6f0ca26e4fb0582e36fc3f98fab456
ljtnono <ljtnono@163.com>
2020-02-21 13:03:07 +0800

    设置值为null的错误信息

--- src/main/java/cn/ljtnono/re/dto/PageDTO.java
+++ src/main/java/cn/ljtnono/re/dto/PageDTO.java
@@ -22,12 +22,12 @@ public class PageDTO implements Serializable {
     private static final long serialVersionUID = -3320275112750620130L;
 
     /** 请求的页数 */
-    @NotNull
+    @NotNull(message = "请求页数不能为null")
     @Min(value = 1, message = "请求页数不能小于1")
     private Integer page;
 
     /** 每页显示的条数 */
-    @NotNull
+    @NotNull(message = "每页显示条数不能为null")
     @Min(value = 10, message = "每页条数不能小于10")
     private Integer count;
 
