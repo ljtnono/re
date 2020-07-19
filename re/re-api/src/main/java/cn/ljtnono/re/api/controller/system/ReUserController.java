e2a1b65ac8b9a5d1ebbb150641286776099822a5
ljtnono <ljtnono@163.com>
2020-07-19 23:15:30 +0800

    修改配置文件

--- re/re-api/src/main/java/cn/ljtnono/re/api/controller/system/ReUserController.java
+++ re/re-api/src/main/java/cn/ljtnono/re/api/controller/system/ReUserController.java
@@ -5,6 +5,9 @@ import cn.ljtnono.re.security.component.ReUserDetailsServiceImpl;
 import cn.ljtnono.re.security.util.ReJwtUtil;
 import cn.ljtnono.re.vo.ReJsonResultVO;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -12,6 +15,11 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * @author ljt
+ * Date 2020/7/16 1:19 上午
+ * Description:
+ */
 @RestController
 @RequestMapping("/api/system/user")
 @Slf4j
@@ -30,13 +38,16 @@ public class ReUserController {
         this.reUserDetailsServiceImpl = reUserDetailsServiceImpl;
     }
 
-
     @GetMapping("/test")
     public void test() {
         log.info("ReUserController执行了");
     }
 
     @PostMapping("/login")
+    @ApiOperation(value = "用户")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "success")
+    })
     public ReJsonResultVO<?> login(@RequestBody ReUser reUser) {
         UsernamePasswordAuthenticationToken upToken = new UsernamePasswordAuthenticationToken(reUser.getUsername(), reUser.getPassword());
         Authentication authenticate = authenticationManager.authenticate(upToken);
