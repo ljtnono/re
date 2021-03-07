a0702dd46c9c0c987b16f9fae2529fc7b483ee47
lingjiatong <935188400@qq.com>
2021-03-08 02:41:51 +0800

    [feat]修改docker启动脚本

--- re/project/scripts/docker_start.sh
+++ re/project/scripts/docker_start.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 docker run -d \
--p 8001:80 \
--p 8002:443 \
+-p 80:80 \
+-p 443:443 \
 -p 8003:3306 \
 -p 8004:6379 \
 -p 8005:9200 \
---name re \
---privileged=true re_base:20210210
+--name re_<version> \
+--privileged=true \
+re:<version> /sbin/init
