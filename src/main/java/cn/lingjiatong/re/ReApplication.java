e7b7a5e8aca29c20ce85c7fde7bde6b561f9304f
lingjiatong <935188400@qq.com>
2023-07-27 22:45:27 +0800

    [feat]删除gradle wapper相关，使用本地gradle

--- src/main/java/cn/lingjiatong/re/ReApplication.java
+++ src/main/java/cn/lingjiatong/re/ReApplication.java
@@ -3,6 +3,12 @@ package cn.lingjiatong.re;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 启动类
+ *
+ * @author lingjiatong
+ * Date: 2023-07-27 22:30
+ */
 @SpringBootApplication
 public class ReApplication {
 
