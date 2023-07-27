c4a08a4079d9b7194fdeca64892149818d6f5504
lingjiatong <935188400@qq.com>
2023-07-27 14:35:58 +0800

    [feat]初始化项目

---
+++ src/main/java/cn/lingjiatong/re/ReApplication.java
@@ -0,0 +1,13 @@
+package cn.lingjiatong.re;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/lingjiatong/re/ReApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lingjiatong.re;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
