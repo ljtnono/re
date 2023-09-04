6a054e0f5619451a3c721019c25978dbcb285a94
lingjiatong <ljtnono@163.com>
2023-09-04 16:02:57 +0800

    [init]初始化项目

---
+++ README.md
@@ -0,0 +1,9 @@
+## 根元素个人博客单机版（基于jdk17、springboot3和gradle构建）
+
+### 项目目录结构
+
+```txt
+re 
+├── re-api                                      // API微服务
+├── re-common                                   // 通用模块
+```
---
+++ re-api/src/main/java/cn/lingjiatong/re/api/ReApiApplication.java
@@ -0,0 +1,18 @@
+package cn.lingjiatong.re.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author Ling, Jiatong
+ * Date: 2023/7/27 23:58
+ */
+@SpringBootApplication
+public class ReApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReApiApplication.class, args);
+    }
+}
